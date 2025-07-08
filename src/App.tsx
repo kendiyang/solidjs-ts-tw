a0faa5e05fc11a6e33931e291f55c8856063d0ae
xKevIsDev <zennerd404@gmail.com>
2025-07-08 23:40:02 +0100

    add: h-screen for display purposes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <div class="bg-white">
+    <div class="bg-white h-screen">
       <p class="text-4xl text-black text-center py-20">Hello World!</p>
       <p class="text-4xl text-black/70 text-center py-20">Start prompting to see changes</p>
     </div>
